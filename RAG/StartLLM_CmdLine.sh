1706b5857966c81d32e62369ade3f5704d5cb1d8
nancynewlin-masi <86989160+nancynewlin-masi@users.noreply.github.com>
2024-12-28 15:30:46 -0600

    Create StartLLM_CmdLine.sh
    Code for downloading and setting up LLM on command line with llama file

---
+++ RAG/StartLLM_CmdLine.sh
@@ -0,0 +1,11 @@
+# Get from online
+wget https://huggingface.co/Mozilla/Llama-3.2-1B-Instruct-llamafile/resolve/main/Llama-3.2-1B-Instruct.Q6_K.llamafile
+
+# Change permissions of file
+chmod +x Llama-3.2-1B-Instruct.Q6_K.llamafile
+
+# Start server
+./Llama-3.2-1B-Instruct.Q6_K.llamafile
+
+# End server
+Ctrl+C
